9e62d9545c1289e022e773a6ce670ef9d3ddb510
Indra <indra@ide.web.id>
2022-11-26 23:24:13 +0700

    add new msisdn

--- src/controller/chat-controller.go
+++ src/controller/chat-controller.go
@@ -32,7 +32,7 @@ func ChatUser(c *fiber.Ctx) error {
 	database.Datasource.DB().Where("msisdn", req.Msisdn).First(&user)
 
 	var chat model.Chat
-	database.Datasource.DB().Joins("Order", database.Datasource.DB().Where(&model.Order{Voucher: req.Voucher})).Where(&model.Chat{UserID: user.ID, IsLeave: false}).Preload("User").Preload("Doctor").Preload("Order").First(&chat)
+	database.Datasource.DB().Joins("Order", database.Datasource.DB().Where(&model.Order{UserID: user.ID, Voucher: req.Voucher})).Preload("User").Preload("Doctor").Preload("Order").First(&chat)
 
 	return c.Status(fiber.StatusOK).JSON(&chat)
 }
